0cdbb87d6ee88dbcee17d1e8ba734ec55761d0f5
O.G. Brown, III <education@ogbrown.com>
2016-02-13 07:31:00 -0600

    HelloWorld 0.0.1 - Getting Started

---
+++ HelloWorldProj/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author O.G. Brown <education@ogbrown.com>
+ * @version: 0.0.1
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
